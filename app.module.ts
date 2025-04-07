790fed873afa631c1086dc95c06bbe024248bf7f
Hema Sri Pulamolu <128466961+Pulamolu@users.noreply.github.com>
2025-04-07 12:59:31 +0530

    Add files via upload
    backend/backend/src/app

--- app.module.ts
+++ app.module.ts
@@ -1,1 +1,24 @@
-// This file is not needed as we're using standalone components 
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TaskListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { } 
